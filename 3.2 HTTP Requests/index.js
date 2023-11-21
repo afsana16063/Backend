2dccb80967f3469a58f0b7eb4a95ce6ca82ca9f0
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2023-11-21 22:55:37 +0400

    HTTP Requests

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(
+    "<h1>Welcome to the simple page. You will learn about me and my contact</h1>"
+  );
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.get("/about", (req, res) => {
+  res.send("<h1>My name is Afsana, I am a Developer&Designer</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h2>You can reach me out my mail, and linkedin</h2>");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
